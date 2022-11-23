283530efbc836fba8ac8bbfc02ce1c30d2b897f2
YATIN-GARG <110814691+YATIN-GARG@users.noreply.github.com>
2022-11-23 19:35:51 +0530

    Create main.java
    driver code

---
+++ main.java
@@ -0,0 +1,14 @@
+// importing required files from package
+import SmartTisch.entity;
+
+
+// driver
+public class main
+{
+  public static void main(String args[])
+  {
+    System.out.println("SmartTisch");
+    
+    // add calls and definitions as per flow
+  }
+}
