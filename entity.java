7909b29da9166677baf44391417bc81830fe4f3a
YATIN-GARG <110814691+YATIN-GARG@users.noreply.github.com>
2022-11-23 19:24:19 +0530

    Create entity.java
    defines classes corresponding to entity objects

---
+++ entity.java
@@ -0,0 +1,43 @@
+// class of entity objects :Table 
+public class Table
+{
+    // add fields
+    
+    // add methods
+    
+    public Table()
+    {
+        // define constructor
+    }
+}
+
+
+// class of entity objects :Guest
+public class Guest
+{
+    // add fields
+    
+    // add methods
+    
+    public Guest()
+    {
+        // define constructor
+    }
+}
+
+
+// association class: realising link of object :Guest and object :Table 
+public class Reservation
+{
+    // add fields
+    
+    // add methods
+    
+    public Reservation(Guest argGuest, Table argTable)
+    {
+        // define constructor
+    }
+}
+
+
+// add more class definitions
