2bdb1dd999b07f1912eb6825182be7aa982df2ff
YATIN-GARG <110814691+YATIN-GARG@users.noreply.github.com>
2022-12-07 22:00:09 +0530

    Update entity.java

--- entity.java
+++ entity.java
@@ -3,7 +3,7 @@ public class Employee
 {
     private String name;
     private String position;
-    
+
     public Employee(String argName, String argPosition)
     {
         name = argName;
@@ -16,18 +16,20 @@ public class Employee
 public class Admin extends Employee
 {
     // add fields
-    
+
     public Guest createGuest(String argName, String argPhoneNumber)
     {
         return Guest(argName, argPhoneNumber);
     }
-    
-    public void createTableList(int argCount, String argSection[], int argDefaultSize[])
+
+    public Vector<Table> createTableList(int argCount, String argSection[], int argDefaultSize[])
     {
+        Vector<Table> TableList = new Vector<Table>();
         for(int index = 0; index < argCount; ++index)
-            Table(argSection[index], argDefaultSize[index]);        // need to store instances
+            TableList.addElement(Table(argSection[index], argDefaultSize[index]));
+        return TableList;
     }
-    
+
     public void deleteTableList()
     {
         // either define destructor or assign NULL to references
@@ -35,35 +37,35 @@ public class Admin extends Employee
 }
 
 
-// class of entity objects :Table 
+// class of entity objects :Table
 public class Table
 {
     static int count;
     static
     {
-        count = 0;            
+        count = 0;
     }
-    
+
     private int tableNumber;
     private String section;
     private int defaultSize;
     private boolean tableStatus;    // signifies whether reserved
-    
+
     public void assignTable()
     {
         tableStatus = true;
     }
-    
+
     public void clearTable()
     {
         tableStatus = false;
     }
-    
+
     public boolean CheckStatus()
     {
         return tableStatus;
     }
-    
+
     public Table(String argSection, int argDefaultSize)
     {
         tableNumber = ++count + 100;        // tableNumber(s) are assigned subsequent numbers beginning from 101
@@ -82,16 +84,16 @@ public class Guest
     {
         count = 0;
     }
-    
+
     private int guestID;
     private String name;
     private String phoneNumber;
-    
+
     // add fields
-    
-    
+
+
     // add methods
-    
+
     public Guest(String argName, String argPhoneNumber)
     {
         guestID = ++count + 1000;       // guestID(s) are assigned subsequent numbers beginning from 1001
@@ -101,7 +103,7 @@ public class Guest
 }
 
 
-// association class: realising link of object :Guest and object :Table 
+// association class: realising link of object :Guest and object :Table
 public class Reservation
 {
     static int count;
@@ -109,32 +111,32 @@ public class Reservation
     {
         count = 0;
     }
-    
+
     private int reservationID;
     private int groupSize;
     private int reservationTime;
     private String sectionPreference;
-    
+
     private Guest visitor;              // Guest who reserves table(s)
     private Vector<Table> tableList;    // List of reserved table(s)
     private int tableCount;             // Count of reserved table(s)
-    
+
     public void createReservation()
     {
         // constructor would serve the purpose
     }
-    
+
     public void cancelReservation()
     {
         // setting the reserved tables free
         for(int index = 0; index < tableCount; ++index)
             tableList[index].tableStatus = false;
     }
-    
+
     // argAlternative signifies the kind of modification
     public boolean modifyReservation(int argAlternative, int argCount = 0, Table argTable[] = NULL)
     {
-        switch(expression) 
+        switch(expression)
         {
             case 1:     // reserve table(s) in addition
                 for(int index = 0; index < argCount; ++index)
@@ -144,26 +146,26 @@ public class Reservation
                 }
                 tableCount += argCount;
                 break;
-            
+
             // add more alternatives
         }
-        
+
         return 0;   // success
     }
-    
+
     // add methods
-    
+
     public Reservation(Guest argGuest, Table[] argTable, int argTableCount, int argGroupSize, int argReservationTime, String argSectionPreference)
     {
         visitor = argGuest;
-        tableList = new Vector();
+        tableList = new Vector<Table>();
         for(int index = 0; index < argTableCount; ++argCount)
         {
             tableList.addElement(argTable[index]);
             argTable[index].tableStatus = true;
         }
         tableCount = argTableCount;
-        
+
         groupSize = argGroupSize;
         reservationTime = argReservationTime;
         sectionPreference = argSectionPreference;
